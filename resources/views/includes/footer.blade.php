1603738f41a3fd5f6b0857b2979285e61e7dfb29
VAKA Marcel <marcelvaka@gmail.com>
2021-01-20 14:40:30 +0000

    project updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,166 +1,43 @@
-{{-- <div class="ui inverted vertical footer segment">
-    <div class="ui container">
-        <div class="ui stackable inverted divided equal height stackable grid">
-            <div class="five wide column">
-                <h4 class="ui inverted header">TRODWE</h4>
-                <div class="ui inverted link list">
-                    <p>TRODWE est la plateforme E-commerce qui propose également des solutions informatiques</p>
-                </div>
-            </div>
-            <div class="five wide column">
-                <h4 class="ui inverted header">Sitemap</h4>
-                <div class="ui inverted link list">
-                    <a href="#" class="item">Boutique</a>
-                    <a href="#" class="item">FAQ</a>
-                    <a href="#" class="item">A propos</a>
-                    <a href="#" class="item">Blog</a>
-                </div>
-            </div>
-            <div class="five wide column">
-                <h4 class="ui inverted header">Suivez-nous sur: </h4>
-                <p> 
-                    <a href="#"><i class="facebook icon"></i></a>
-                    <a href="#"><i class="instagram icon"></i></a>
-                    <a href="#"><i class="twitter icon"></i></a>
-                    <a href="#"><i class="linkedin icon"></i></a>
-                </p>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
-{{-- <div class="footer_top">
-	<div class="span_of_4">
-		<div class="col-md-3 box_4">
-			<h4>Shop</h4>
-			<ul class="f_nav">
-				<li><a href="#">new arrivals</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">trends</a></li>
-				<li><a href="#">sale</a></li>
-				<li><a href="#">style videos</a></li>
-			</ul>	
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>help</h4>
-			<ul class="f_nav">
-				<li><a href="#">frequently asked  questions</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-			</ul>				
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>account</h4>
-			<ul class="f_nav">
-				<li><a href="#">login</a></li>
-				<li><a href="#">create an account</a></li>
-				<li><a href="#">create wishlist</a></li>
-				<li><a href="#">my shopping bag</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">create wishlist</a></li>
-			</ul>
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>popular</h4>
-			<ul class="f_nav">
-				<li><a href="#">new arrivals</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">trends</a></li>
-				<li><a href="#">sale</a></li>
-				<li><a href="#">style videos</a></li>
-				<li><a href="#">login</a></li>
-				<li><a href="#">brands</a></li>
-			</ul>			
-		</div>
-		<div class="clearfix"></div>
-	</div>
-    <!-- start span_of_2 -->
-		<div class="span_of_2">
-			<div class="span1_of_2">
-				<h5>need help? <a href="#">contact us <span> &gt;</span> </a> </h5>
-				<p>(or) Call us: +22-34-2458793</p>
-			</div>
-			<div class="span1_of_2">
-				<h5>follow us </h5>
-				<div class="social-icons">
-					     <ul>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li class="last2"><a href="#" target="_blank"></a></li>
-						</ul>
-				</div>
-			</div>
-			<div class="clearfix"></div>
-		<div class="copy">
-		   <p>© 2015 All Rights Reseverd Template by <a href="http://w3layouts.com/">W3layouts</a> </p>
-		</div>
-		</div> --}}
 
 <div class="footer_top">
 	    <div class="span_of_4">
             <div class="col-md-3 box_4">
-                <h4>Shop</h4>
+                <h4>Boutique</h4>
                 <ul class="f_nav">
-                    <li><a href="#">new arrivals</a></li>
-                    <li><a href="#">men</a></li>
-                    <li><a href="#">women</a></li>
-                    <li><a href="#">accessories</a></li>
-                    <li><a href="#">kids</a></li>
-                    <li><a href="#">brands</a></li>
-                    <li><a href="#">trends</a></li>
-                    <li><a href="#">sale</a></li>
-                    <li><a href="#">style videos</a></li>
+                    <li><a href="#">Ordinateurs portables</a></li>
+                    <li><a href="#">Bureautiques</a></li>
+                    <li><a href="#">Téléphones portables</a></li>
+                    <li><a href="#">Tablettes</a></li>
+                    <li><a href="#">TVs</a></li>
+                    <li><a href="#">Caméras digital</a></li>
+                    <li><a href="#">Electro-menager</a></li>
                 </ul>	
             </div>
             <div class="col-md-3 box_4">
-                <h4>help</h4>
+                <h4>Aide</h4>
                 <ul class="f_nav">
-                    <li><a href="#">frequently asked  questions</a></li>
-                    <li><a href="#">men</a></li>
-                    <li><a href="#">women</a></li>
-                    <li><a href="#">accessories</a></li>
-                    <li><a href="#">kids</a></li>
-                    <li><a href="#">brands</a></li>
+                    <li><a href="#">FAQs</a></li>
                 </ul>				
             </div>
             <div class="col-md-3 box_4">
-                <h4>account</h4>
+                <h4>Comptes</h4>
                 <ul class="f_nav">
-                    <li><a href="#">login</a></li>
-                    <li><a href="#">create an account</a></li>
-                    <li><a href="#">create wishlist</a></li>
-                    <li><a href="#">my shopping bag</a></li>
-                    <li><a href="#">brands</a></li>
-                    <li><a href="#">create wishlist</a></li>
+                    <li><a href="#">Se connecter</a></li>
+                    <li><a href="#">Créer un compte</a></li>
+                    <li><a href="#">Créer une liste de favoris</a></li>
+                    <li><a href="#">Mon panier</a></li>
+                    <li><a href="#">Marques</a></li>
                 </ul>
             </div>
             <div class="col-md-3 box_4">
-                <h4>popular</h4>
+                <h4>Populaire</h4>
                 <ul class="f_nav">
-                    <li><a href="#">new arrivals</a></li>
-                    <li><a href="#">men</a></li>
-                    <li><a href="#">women</a></li>
-                    <li><a href="#">accessories</a></li>
-                    <li><a href="#">kids</a></li>
-                    <li><a href="#">brands</a></li>
-                    <li><a href="#">trends</a></li>
-                    <li><a href="#">sale</a></li>
-                    <li><a href="#">style videos</a></li>
-                    <li><a href="#">login</a></li>
-                    <li><a href="#">brands</a></li>
+                    <li><a href="#">Nouvel arrivage</a></li>
+                    <li><a href="#">Ordinateur</a></li>
+                    <li><a href="#">Téléphones portables</a></li>
+                    <li><a href="#">Accessoires</a></li>
+                    <li><a href="#">Marques</a></li>
+                    <li><a href="#">Les plus vendus</a></li>
                 </ul>			
             </div>
             <div class="clearfix"></div>
@@ -168,11 +45,11 @@
 		<!-- start span_of_2 -->
 		<div class="span_of_2">
 			<div class="span1_of_2">
-				<h5>need help? <a href="#">contact us <span> &gt;</span> </a> </h5>
-				<p>(or) Call us: +22-34-2458793</p>
+				<h5>Avez-vous besoin d'aide ? </h5>
+				<p>(ou) Appelez le: +225 78 914 610</p>
 			</div>
 			<div class="span1_of_2">
-				<h5>follow us </h5>
+				<h5><a href="#">contactez-nous <span> &gt;</span> </a> </h5>
 				<div class="social-icons">
 					     <ul>
 					        <li><a href="#" target="_blank"></a></li>
@@ -186,6 +63,6 @@
 			<div class="clearfix"></div>
 		</div>
 		<div class="copy">
-		   <p>© 2015 All Rights Reseverd Template by <a href="http://w3layouts.com/">W3layouts</a> </p>
+		   <p>© 2021 Tous droits réservés par <a href="#">VAKA Group</a> </p>
 		</div>
      </div>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -17,78 +17,6 @@
 </div>
 
 <div class="ui divider"></div>
-
-{{-- <div class="main ui container">
-    <div class="ui grid">
-        <div class="three wide column">
-            <h4 class="ui header">Par Categories</h4>
-            <div class="ui list">
-                @foreach ($categories as $category)
-                <a class="item" href="{{ route('shop.index', ['category' => $category->slug]) }}">
-                    <div class="@if(request()->category == $category->slug) header @endif">{{ $category->name }}</div>
-                </a>
-                @endforeach
-            </div>
-        </div>
-
-        <div class="thirteen wide column">
-
-            <div class="ui menu">
-                <div class="ui dropdown item">
-                    Tri Par prix
-                    <div class="menu">
-                        <a class="item" href="{{ route('shop.index', ['category' => request()->category, 'sort' => 'low_high']) }}">
-                            Plus Bas -> Plus Grand
-                        </a>
-                        <a class="item" href="{{ route('shop.index', ['category' => request()->category, 'sort' => 'high_low']) }}">
-                            Plus Grand -> Plus Bas
-                        </a>
-                    </div>
-                </div>
-
-                <div class="right menu">
-                    <div class="item">
-                        <div class="ui transparent icon input">
-                            <input type="text" placeholder="Search within category...">
-                            <i class="search link icon"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            @if($products->isNotEmpty())
-
-            <div class="ui three doubling stackable link cards">
-
-                @foreach ($products as $product)
-
-                @include('partials.product')
-
-                @endforeach
-
-            </div>
-
-            @else
-
-            <h2 class="ui icon center aligned header">
-                <i class="info circle icon"></i>
-                <div class="content">
-                    Sorry! No Products Found..
-                    <div class="sub header">Try search on different categories.</div>
-                </div>
-            </h2>
-
-            @endif
-
-            <div class="ui divider"></div>
-
-            {{ $products->appends(request()->input())->links() }}
-
-        </div>
-
-    </div>
-</div> --}}
-
 <div class="content">
   <div class="content_box">
 	<div class="men">
@@ -112,67 +40,10 @@
         @endforeach
 	</ol>
 </dd>
-    {{-- <dt class="even">Manufacturer</dt>
-<dd class="even">
-<ol>
-    <li>
-        <a href="#">Calvin Klein</a>(2)
-    </li>
-    <li>
-        <a href="#">Diesel</a>(1)
-    </li>
-    <li>
-        <a href="#">Polo</a>(1)
-    </li>
-    <li>
-        <a href="#">Tommy Hilfiger</a>(1)
-    </li>
-    <li>
-        <a href="#">Versace</a>(1)
-    </li>
-</ol>
-</dd> --}}
-        {{-- <dt class="last odd">Color</dt>
-<dd class="last odd">
-<ol>
-    <li>Black (0)</li>
-    <li>
-        <a href="#">Blue</a>(2)
-    </li>
-    <li>
-        <a href="#">Green</a>(1)
-    </li>
-    <li>
-        <a href="#">Grey</a>(1)
-    </li>
-    <li>
-        <a href="#">Red</a> (1)
-    </li>
-    <li>
-        <a href="#">White</a>(1)
-    </li>
-</ol>
-</dd> --}}
     </dl>
 
     </div>
 </div>
-{{-- <div class="block block-cart">
-        <div class="block-title">
-        <strong><span> Carrello </span></strong>
-    </div>
-    <div class="block-content">
-        <p class="empty"> You have no items in your shopping cart. </p>
-    </div>
-</div>
-<div class="block block-list block-compare">
-    <div class="block-title">
-        <strong><span> Compare Products </span></strong>
-    </div>
-    <div class="block-content">
-        <p class="empty"> You have no items to compare.</p>
-    </div>
-</div> --}}
 </div>
 <div class="col-md-9">
 	<div class="mens-toolbar">
@@ -190,19 +61,7 @@
 		        </div>
     		</div>
 	        <div class="pager">
-	            {{-- <div class="liter visible-desktop">
-					<label>Show</label>
-					<select>
-	                    <option value="" selected="selected"> 9 </option>
-	                    <option value=""> 15 </option>
-	                    <option value=""> 30 </option>
-	                </select> per page
-	            </div> --}}
-	       		{{-- <ul class="dc_pagination dc_paginationA dc_paginationA06">
-				    <li><a href="#" class="previous">Pages</a></li>
-				    <li><a href="#">1</a></li>
-				    <li><a href="#">2</a></li>
-			  	</ul> --}}
+	            
 		   		<div class="clearfix"></div>
 	    	</div>
      	    <div class="clearfix"></div>
